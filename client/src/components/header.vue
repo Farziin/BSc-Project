f5dbdb82c7b9de76770ac12fe1ff6104ea51a2dc
Farziin <farzin.heidary@gmail.com>
2018-08-11 22:16:41 +0430

    add scenario page added

---
+++ client/src/components/header.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <b-container fluid>
+    <b-modal v-model="modalShow" hide-footer>
+      <p class="text-center">لطفاً ابتدا وارد سایت شوید.</p>
+    </b-modal>
+
+    <b-navbar toggleable="md" type="dark" variant="dark">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand>سناریوساز</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#/login" v-if="showName === false">ورود</b-nav-item>
+          <b-nav-item v-if="showName === true">سلام {{ $store.state.name }} عزیز</b-nav-item>
+          <b-nav-item v-on:click="addScenario">ایجاد سناریوی جدید</b-nav-item>
+          <b-nav-item href="#">ویرایش سناریوی موجود</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showName: false,
+      modalShow: false
+    }
+  },
+  created: function () {
+    if (this.$store.state.token !== null) {
+      this.showName = true
+    } else {
+      this.showName = false
+    }
+  },
+  methods: {
+    addScenario: function () {
+      if (this.$store.state.token === null) {
+        this.modalShow = true
+      } else {
+        this.$router.push('/add-scenario')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/pages/add-scenario.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-container fluid>
+    <header-part></header-part>
+    <br class="empty-space"/>
+    <!-- <scenario-name :name.sync="scenarioName"></scenario-name> -->
+    <things-table v-if="pageNumber === 1"></things-table>
+    <br class="empty-space"/>
+    <b-row align-h="center">
+      <b-button-group>
+        <b-button variant="info" class="pageination-btn" v-if="pageNumber > 1">مرحله قبلی</b-button>
+        <b-button variant="primary" class="pageination-btn">مرحله {{ pageNumber }}</b-button>
+        <b-button variant="info" class="pageination-btn">مرحله بعدی</b-button>
+      </b-button-group>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import HeaderPart from '../components/header'
+import ThingsTable from '../components/things-table'
+// import ScenarioName from '../components/scenario-name'
+
+export default {
+  components: {
+    HeaderPart,
+    ThingsTable
+    // ScenarioName
+  },
+  data () {
+    return {
+      pageNumber: 1
+      // scenarioName: 'test'
+    }
+  }
+}
+</script>
+
+<style>
+.empty-space {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+
+.pageination-btn {
+  border-radius: 0px;
+}
+</style>
